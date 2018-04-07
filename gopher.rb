f134d84c58c90848a3accdf29230bbf5d127e585
Dillon Welch <daw0328@gmail.com>
2018-04-07 13:12:50 -0700

    Add join

--- gopher.rb
+++ gopher.rb
@@ -1,12 +1,16 @@
 # frozen_string_literal: true
 def next_coordinate(x, y, size)
-  'hi'
+  @map[x][y] = 1
 end
 
 num_tests = gets.chomp.to_i
 
 num_tests.times do |test|
   size = gets.chomp.to_i
+  @map = Array.new(1000) { Array.new(1000, 0) }
+  old_x = 499
+  old_y = 499
+  result = []
   puts "500 500"
   while true do
     line = gets.chomp.split
@@ -14,6 +18,9 @@ num_tests.times do |test|
     y = line[1].to_i
     exit if x == y && x == -1
     break if x == y && x == 0
-    puts next_coordinate(x, y, size)
+    result = next_coordinate(x - 1, y - 1, size)
+    puts result.join(' ')
+    old_x = x
+    old_y = y
   end
 end
