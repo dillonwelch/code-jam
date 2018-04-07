15ff6c6fd1c9db9dd02e94c7bafc6c2842465d69
Dillon Welch <daw0328@gmail.com>
2018-04-07 11:58:21 -0700

    Basic loop for length 2

--- saving_universe.rb
+++ saving_universe.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 def evaluate(commands)
-  arr = commands.split('')
   damage = 1
   total_damage = 0
 
-  arr.each do |command|
+  commands.each do |command|
     if command == 'S'
       total_damage += damage
     else
@@ -19,12 +18,38 @@ end
 def hack(commands, shield)
   total_damage = evaluate(commands)
   if total_damage > shield
-    # TODO hack
+    if commands.length == 2
+      result = [commands[1], commands[0]]
+      if evaluate(result) > shield
+        return false
+      else
+        return result
+      end
+    end
   else
-    return true
+    return 0
   end
 end
 
+def number_of_hacks(commands, shield, case_number)
+  counter = 0
+  while true
+    result = hack(commands, shield)
+    if result.is_a? Array
+      counter += 1
+      commands = result
+    else
+      case result
+      when 0
+        puts "Case ##{case_number}: #{counter}"
+        return
+      when false
+        puts "Case ##{case_number}: IMPOSSIBLE"
+        return
+      end
+    end
+  end
+end
 
-
-puts "Case #1: #{hack('CS', 2)}"
+number_of_hacks('CS'.split(''), 1, 1)
+number_of_hacks('CS'.split(''), 2, 2)
